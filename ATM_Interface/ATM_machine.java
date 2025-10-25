f84e02b1dd19a3b5e643800a9a7588153f1190cf
Yashroy123 <157899779+Yashroy123@users.noreply.github.com>
2025-10-25 11:30:56 -0700

    Update ATM_machine.python

--- ATM_Interface/ATM_machine.java
+++ ATM_Interface/ATM_machine.java
@@ -1,144 +1,106 @@
-package ATM;
-import java.util.Scanner;
+class ATM:
+    def __init__(self, saving, name, acc_no):
+        self.name = name
+        self.account_no = acc_no
+        self.saving = saving
 
-  class ATM {
-     String name;
-     int account_no;
-     int saving ;
+    def greeting(self):
+        print("Hello:", self.name)
+        print("Your Bank account no is:", self.account_no)
 
-      ATM(int saving,String name,int Acc_no) {
-         this.name = name;
-         this.account_no = Acc_no;
-         this.saving=saving;
-     }
+    def deposit(self, amount):
+        if amount >= 0:
+            self.saving += amount
+            print("Amount deposited successfully.")
+        else:
+            print("Invalid Input, try again.")
 
+    def withdraw(self, amount):
+        if amount <= 0 or amount > self.saving:
+            print("Insufficient balance or invalid amount.")
+        else:
+            self.saving -= amount
+            print("Transaction successful.")
 
-     public void greeting() {
-         System.out.println("Hello:" + name);
-         System.out.println("Your Bank account no is :" + account_no);
-     }
-
-     public void deposit(int amount) {
-        if(amount>=0) {
-            saving += amount;
-            System.out.println("Amount deposit Successfully:");
-        }
-         System.out.println("Invalid Input,try again");
-
-     }
-
-     public void withdraw(int amount) {
-         if (amount < 0 || amount>saving) {
-             System.out.println("Insufficient balance:");
-         }
-         saving = saving - amount;
-         System.out.println("Transaction successful:");
-
-     }
-
-     public void check_balance() {
-         System.out.println("current balance is:"+saving);
-     }
+    def check_balance(self):
+        print("Current balance is:", self.saving)
 
 
- }
- class user_bank_account {
-         private ATM balance;
-         Scanner sc;
-        user_bank_account(ATM balance){
-            this.balance=balance;
-            this.sc=new Scanner(System.in);
-            balance.greeting();
-        }
+class UserBankAccount:
+    def __init__(self, atm):
+        self.balance = atm
+        self.balance.greeting()
+
+    def menu(self):
+        terminate = False
+        while not terminate:
+            print("\nSelect Options:")
+            print("1. Withdrawal")
+            print("2. Deposit")
+            print("3. Check Balance")
+            print("4. Exit")
+
+            option = int(input("Enter your choice: "))
+
+            if option == 1:
+                self.withdraw()
+            elif option == 2:
+                self.deposit()
+            elif option == 3:
+                self.check_balance()
+            elif option == 4:
+                terminate = True
+                print("Have a nice day, thanks for choosing our bank!")
+            else:
+                print("Invalid option.")
+
+    def withdraw(self):
+        amount = int(input("Enter the amount: "))
+        self.balance.withdraw(amount)
+
+    def deposit(self):
+        amount = int(input("Enter the amount: "))
+        self.balance.deposit(amount)
+
+    def check_balance(self):
+        self.balance.check_balance()
 
 
-        public void menu() {
-            boolean terminiate = false;
-            while (!terminiate) {
-                Scanner scanner=new Scanner(System.in);
-                System.out.println("Select Options:");
-                System.out.println("1.Withdrawal:");
-                System.out.println("2.deposit:");
-                System.out.println("3.Check_balance:");
-                System.out.println("4.Exit:");
-                int option = scanner.nextInt();
-                switch (option){
-                    case 1:
-                      withdraw();
-                      break;
-                    case 2:
-                        deposit();
-                        break;
-                    case 3:
-                       check_balancee();
-                       break;
-                    case 4:
-                        terminiate=true;
-                        System.out.println("Have a nice day,thanks for choosing our bank:");
-                        break;
-                    default:
-                        System.out.println("Invalid option:");
-                }
-            }
-        }
+def main():
+    print("== Welcome to Union Bank ==")
+    attempt = 0
+    make = False
 
-        public void withdraw(){
-            System.out.println(("Enter the amount:"));
-            int amount=sc.nextInt();
-           balance.withdraw(amount);
-        }
+    while not make:
+        pin = int(input("Enter Your Bank Account PIN: "))
 
-         public void deposit(){
-             System.out.println("Enter the amount:");
-            int amount=sc.nextInt();
-            balance.deposit(amount);
-         }
-         public void check_balancee(){
-            balance.check_balance();
-         }
- }
+        if pin == 123:
+            obj = ATM(25000, "Yash kumar roy", 123688)
+            bank = UserBankAccount(obj)
+            bank.menu()
+        elif pin == 415:
+            obj = ATM(50000, "Manish kumar roy", 12334688)
+            bank = UserBankAccount(obj)
+            bank.menu()
+        elif pin == 768:
+            obj = ATM(35000, "Lakshya kumar roy", 123688)
+            bank = UserBankAccount(obj)
+            bank.menu()
+        elif pin == 543:
+            obj = ATM(45000, "Prince kumar roy", 123688)
+            bank = UserBankAccount(obj)
+            bank.menu()
+        else:
+            attempt += 1
+            if attempt >= 3:
+                make = True
+                print("Try Again After Some Time, You Exceeded Maximum Attempts.")
+            else:
+                print("Invalid PIN, Try Again.")
 
 
-    class ATM_machine {
+if __name__ == "__main__":
+    main()
 
-        public static void main(String[] args) {
-            System.out.println("==Welcome to Union Bank===");
-            int attempt=0;
-            Scanner sc = new Scanner(System.in);
-            boolean make=false;
-            while(!make){
-                System.out.println("Enter Your Bank Account Pin:");
-                int pin=sc.nextInt();
-                switch(pin){
-                    case 123:
-                        ATM obj = new ATM(25000, "Yash kumar roy", 123688);
-                        user_bank_account bank = new user_bank_account(obj);
-                        bank.menu();
-                        break;
-                    case 415:
-                        ATM obj1 = new ATM(50000, "Manish kumar roy", 12334688);
-                        user_bank_account bank1 = new user_bank_account(obj1);
-                        bank1.menu();
-                        break;
-                    case 768:
-                        ATM obj2 = new ATM(35000, "lakshya kumar roy", 123688);
-                        user_bank_account bank2 = new user_bank_account(obj2);
-                        bank2.menu();
-                        break;
-                    case 543:
-                        ATM obj3 = new ATM(45000, "Prince kumar roy", 123688);
-                        user_bank_account bank3 = new user_bank_account(obj3);
-                        bank3.menu();
-                        break;
-                    default:
-                        if(attempt==3){
-                            make=true;
-                            System.out.println("Try Again After some time,you exceed maximum attempt:");
-                        }
-                }
-                attempt+=1;
-            }
 
-        }
-    }
 
